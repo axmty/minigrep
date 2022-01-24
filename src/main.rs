44fbe5cce4a7f2cea01ae9117097e7df4e7d0ceb
Alexandre Metgy <alexandre.metgy@gmail.com>
2022-01-24 20:15:28 +0100

    Implement minigrep command line program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("in file {}:", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
